cbc07a47f2b4b1ee0519873cb7842433814728b9
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-05-20 19:42:56 +0200

    Rails Task Manager 1/2/3/4/5/6/demi7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,22 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
